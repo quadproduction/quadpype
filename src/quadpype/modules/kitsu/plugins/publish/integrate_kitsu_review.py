24575ec9d6cb7ef5a1df0fb790c7f7124211e3d3
Cyprien CAILLOT <cyprien.caillot@quad.fr>
2024-11-04 13:18:19 +0100

    Import kitsu-review changes

--- src/quadpype/modules/kitsu/plugins/publish/integrate_kitsu_review.py
+++ src/quadpype/modules/kitsu/plugins/publish/integrate_kitsu_review.py
@@ -4,6 +4,7 @@ import gazu
 import pyblish.api
 
 from quadpype.pipeline.anatomy import Anatomy
+from quadpype.lib.transcoding import VIDEO_EXTENSIONS
 
 
 class IntegrateKitsuReview(pyblish.api.InstancePlugin):
@@ -35,6 +36,23 @@ class IntegrateKitsuReview(pyblish.api.InstancePlugin):
 
             filenames = representation.get("files")
 
+            review_path = representation.get("published_path")
+            if not review_path:
+                raise ValueError("No publish path found in representation.")
+
+            self.log.debug("Processing review representation data : {}".format(review_path))
+
+            review_data_extension = representation.get("ext")
+            if not review_data_extension:
+                raise ValueError("No extension specified in representation ('{}')".format(review_path))
+
+            if f".{review_data_extension}" in VIDEO_EXTENSIONS:
+                gazu.task.add_preview(
+                    task_id, comment_id, review_path, normalize_movie=getattr(self, 'normalize', True)
+                )
+                self.log.info("Review uploaded on comment")
+                continue
+
             export_frames = instance.data.get("exportFrames", [])
             frame_start = instance.data["frameStart"]
             frame_end = instance.data["frameEnd"]
@@ -43,26 +61,15 @@ class IntegrateKitsuReview(pyblish.api.InstancePlugin):
             if not isinstance(filenames, list):
                 filenames = [filenames]
 
-            extension = representation.get("ext")
-            if not extension:
-                self.log.warning("No extension found in representation.")
-                raise IndexError
-
-            review_path = representation.get("published_path")
-            if not review_path:
-                self.log.warning("No publish path found in representation.")
-                raise IndexError
-            self.log.debug("Found review at: {}".format(review_path))
-
             if not export_frames:
                 export_frames = list(range(frame_start, frame_end + 1))
 
             frame_padding = Anatomy().templates.get('frame_padding', 4)
             frame_file_format = f"{{:0{frame_padding}d}}.{{}}"
             if "burnin" in representation.get("tags", []):
-                filenames = [frame_file_format.format(index, extension) for index in export_frames]
+                filenames = [frame_file_format.format(index, review_data_extension) for index in export_frames]
 
-            subtract_pattern = rf"\d{{{frame_padding}}}\.{re.escape(extension)}"
+            subtract_pattern = rf"\d{{{frame_padding}}}\.{re.escape(review_data_extension)}"
             for filename in filenames:
                 image_filepath = re.sub(subtract_pattern, filename, review_path)
                 self.log.info(image_filepath)
