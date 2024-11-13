1bdb0a51278fe157b490335764f661d732df4d06
Cyprien CAILLOT <cyprien.caillot@quad.fr>
2024-11-13 16:12:48 +0100

    update transfer_settings.js

--- src/tools/_lib/database/transfer_settings.js
+++ src/tools/_lib/database/transfer_settings.js
@@ -5,9 +5,14 @@ function transferSettings(sourceDbName, targetDbName) {
     const sourceSettings = sourceDb.getCollection('settings');
     const targetSettings = targetDb.getCollection('settings');
 
+    // Delete the target settings collection if it exists
+    if (targetDb.getCollectionNames().includes('settings')) {
+        targetDb.getCollection('settings').drop();
+    }
+
     // Find all documents in source.settings except "local_settings" and "versions_order"
     settingsDocuments = sourceSettings.find({
-        type: { $nin: ["local_settings", "versions_order", "last_opened_settings_ui"] }
+        type: { $nin: ["local_settings", "versions_order"] }
     }).toArray();
 
     latestVersionedSettings = {};
@@ -31,11 +36,21 @@ function transferSettings(sourceDbName, targetDbName) {
             document.version = "4.0.0";
         }
 
+        if (document.type === "system_settings"){
+            document.type = "global_settings";
+            document.data.core = document.data.general;
+            delete document.data.general;
+            targetSettings.insert(document);
+            return
+        }
+
         if (document.type === "global_settings") {
-            document.production_version = "4.0.0";
-            document.staging_version = "4.0.0";
+            document.type = "core_settings";
+            document.data.production_version = "4.0.0";
+            document.data.staging_version = "4.0.0";
             document.data.quadpype_path = document.data.openpype_path;
             delete document.data.openpype_path;
+            targetSettings.insert(document);
             return
         }
 
@@ -46,17 +61,20 @@ function transferSettings(sourceDbName, targetDbName) {
                     latestVersionedAnatomy = document;
                 }
             }
-            return;
+            return
         }
 
         if (document.type === "system_settings_versioned") {
+            document.type = "global_settings_versioned"
+            document.data.core = document.data.general;
+            delete document.data.general;
             if (document.last_saved_info && document.last_saved_info.timestamp) {
                 const timestamp = new Date(document.last_saved_info.timestamp);
                 if (!latestVersionedSystemSettings || timestamp > new Date(latestVersionedSystemSettings.last_saved_info.timestamp)) {
                     latestVersionedSystemSettings = document;
                 }
             }
-            return;
+            return
         }
 
 
@@ -74,10 +92,8 @@ function transferSettings(sourceDbName, targetDbName) {
                     }
                 }
             }
-            return;
+            return
         }
-
-        targetSettings.insert(document);
     });
 
     // Insert Latest Versioned Anatomy Settings
