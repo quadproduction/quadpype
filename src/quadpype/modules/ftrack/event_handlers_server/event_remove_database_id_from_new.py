0dd7544bebe41d2fb983a2ae7591bcb19e9f946b
Ben Souchet <contact@bensouchet.dev>
2024-11-13 09:35:30 +0100

    Rename Avalon in filenames

--- src/quadpype/modules/ftrack/event_handlers_server/event_remove_database_id_from_new.py
+++ src/quadpype/modules/ftrack/event_handlers_server/event_remove_database_id_from_new.py
@@ -1,37 +1,37 @@
 from quadpype_modules.ftrack.lib import BaseEvent
 from quadpype_modules.ftrack.lib.database_sync import CUST_ATTR_ID_KEY
 from quadpype_modules.ftrack.event_handlers_server.event_sync_to_database import (
-    SyncToQuadPypeEvent
+    SyncToDatabaseEvent
 )
 
 
-class DelQuadPypeIdFromNew(BaseEvent):
-    '''
+class RemoveDatabaseIdFromNew(BaseEvent):
+    """
     This event removes QuadPypeId from custom attributes of new entities
     Result:
     - 'Copy->Pasted' entities won't have same QuadPypeID as source entity
 
-    Priority of this event must be less than SyncToQuadPype event
-    '''
-    priority = SyncToQuadPypeEvent.priority - 1
+    Priority of this event must be less than SyncToDatabase event
+    """
+    priority = SyncToDatabaseEvent.priority - 1
     ignore_me = True
 
     def launch(self, session, event):
         created = []
-        entities = event['data']['entities']
+        entities = event["data"]["entities"]
         for entity in entities:
             try:
-                entity_id = entity['entityId']
+                entity_id = entity["entityId"]
 
-                if entity.get('action', None) == 'add':
-                    id_dict = entity['changes']['id']
+                if entity.get("action", None) == "add":
+                    id_dict = entity["changes"]["id"]
 
-                    if id_dict['new'] is not None and id_dict['old'] is None:
-                        created.append(id_dict['new'])
+                    if id_dict["new"] is not None and id_dict["old"] is None:
+                        created.append(id_dict["new"])
 
                 elif (
-                    entity.get('action', None) == 'update' and
-                    CUST_ATTR_ID_KEY in entity['keys'] and
+                    entity.get("action", None) == "update" and
+                    CUST_ATTR_ID_KEY in entity["keys"] and
                     entity_id in created
                 ):
                     ftrack_entity = session.get(
@@ -50,5 +50,5 @@ class DelQuadPypeIdFromNew(BaseEvent):
 
 
 def register(session):
-    '''Register plugin. Called when used as an plugin.'''
-    DelQuadPypeIdFromNew(session).register()
+    """Register plugin. Called when used as a plugin."""
+    RemoveDatabaseIdFromNew(session).register()
