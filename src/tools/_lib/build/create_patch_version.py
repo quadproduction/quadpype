1a4a15f21128ab52e0b3e35889402b0004835a51
Ben Souchet <contact@bensouchet.dev>
2024-11-26 10:40:51 +0100

    Remove _print function in create_patch_version script

--- src/tools/_lib/build/create_patch_version.py
+++ src/tools/_lib/build/create_patch_version.py
@@ -30,37 +30,15 @@ def main(path):
     else:
         out_dir_path = Path(user_data_dir("quadpype", "quad"))
 
-    _print(f"Creating the patch zip archive in {out_dir_path} ...")
+    print(f"Creating the patch zip archive in {out_dir_path} ...")
     version = create_version_from_live_code(progress_callback=update_progress)
 
     progress_bar.close(clear=True)
     if not version:
-        _print("Error while creating the patch zip archive.", 1)
+        print("Error while creating the patch zip archive.")
         exit(1)
 
-    _print(f"Successfully created patch archive v{version}")
-
-
-def _print(msg: str, message_type: int = 0) -> None:
-    """Print message to console.
-
-    Args:
-        msg (str): message to print
-        message_type (int): type of message (0 info, 1 error, 2 note)
-
-    """
-    if not bootstrap.term:
-        header = ""
-    elif message_type == 0:
-        header = bootstrap.term.aquamarine3(">>> ")
-    elif message_type == 1:
-        header = bootstrap.term.orangered2("!!! ")
-    elif message_type == 2:
-        header = bootstrap.term.tan1("... ")
-    else:
-        header = bootstrap.term.darkolivegreen3("--- ")
-
-    print(f"{header}{msg}")
+    print(f"Successfully created patch archive v{version}")
 
 
 if __name__ == "__main__":
