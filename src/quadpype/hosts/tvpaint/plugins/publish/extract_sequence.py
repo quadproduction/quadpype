925cce172d62e062584cc9cd07d818bf14e694e0
Cyprien CAILLOT <cyprien.caillot@quad.fr>
2024-12-19 14:15:42 +0100

    Fix extract_sequence.py

--- src/quadpype/hosts/tvpaint/plugins/publish/extract_sequence.py
+++ src/quadpype/hosts/tvpaint/plugins/publish/extract_sequence.py
@@ -383,34 +383,36 @@ class ExtractSequence(pyblish.api.Extractor):
             output_filepaths_by_frame_index[frame_index] = filepath
 
             # Ensure the frame has been properly created
+            if os.path.exists(filepath):
+                continue
+
+            if export_type == "camera" and frame_index == mark_out:
+                # There is a bug in TVPaint, it seems to NOT render the last frame in camera mode sometimes
+
+                # TODO: We should recreate a clean george_script_lines with only info to render the last frame
+                # Replace the export filepath
+                last_frame_filename = filename_template.format(frame=mark_out)
+                george_script_lines = re.sub(origin_first_filename, last_frame_filename, george_script_lines)
+
+                # set the mark in to mark_out
+                if "tv_markin" not in george_script_lines:
+                    george_script_lines = "{}\n{}".format("tv_markin {}".format(mark_out), george_script_lines)
+                    george_script_lines = "{}\n{}".format(george_script_lines,
+                                                          "tv_markin {}".format(origin_mark_in))
+                else:
+                    george_script_lines = re.sub("tv_markin {}".format(mark_in),
+                                                 "tv_markin {}".format(mark_out),
+                                                 george_script_lines, count=1)
+
+                george_script_lines = re.sub(tv_export, tv_export.replace(str(export_lenght), "0"),
+                                             george_script_lines)
+
+                execute_george_through_file(george_script_lines)
+
+            # If the frame has still not been generated,
+            # this is a true issue we need to crash and notify the user
             if not os.path.exists(filepath):
-                if export_type == "camera" and frame_index == mark_out:
-                    # There is a bug in TVPaint, it seems to NOT render the last frame in camera mode sometimes
-
-                    # TODO: We should recreate a clean george_script_lines with only info to render the last frame
-                    # Replace the export filepath
-                    last_frame_filename = filename_template.format(frame=mark_out)
-                    george_script_lines = re.sub(origin_first_filename, last_frame_filename, george_script_lines)
-
-                    # set the mark in to mark_out
-                    if "tv_markin" not in george_script_lines:
-                        george_script_lines = "{}\n{}".format("tv_markin {}".format(mark_out), george_script_lines)
-                        george_script_lines = "{}\n{}".format(george_script_lines,
-                                                              "tv_markin {}".format(origin_mark_in))
-                    else:
-                        george_script_lines = re.sub("tv_markin {}".format(mark_in),
-                                                     "tv_markin {}".format(mark_out),
-                                                     george_script_lines, count=1)
-
-                    george_script_lines = re.sub(tv_export, tv_export.replace(str(export_lenght), "0"),
-                                                 george_script_lines)
-
-                    execute_george_through_file(george_script_lines)
-
-                # If the frame has still not been generated,
-                # this is a true issue we need to crash and notify the user
-                if not os.path.exists(filepath):
-                    raise KnownPublishError("Output ('{}') was not rendered and exported to disk.".format(filepath))
+                raise KnownPublishError("Output ('{}') was not rendered and exported to disk.".format(filepath))
 
         thumbnail_filepath = None
         if output_filepaths_by_frame_index:
