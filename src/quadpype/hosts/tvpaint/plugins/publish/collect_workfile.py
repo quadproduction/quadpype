94aa834fd60e11fdd9751ff84db07e6b8c95c46c
hfarre <hadrien.farre@wizz.fr>
2025-03-06 16:52:49 +0100

    Rework, on creer maintenant un temp workfile afin d'assurer que les modif sont bien publish dans le workfile de publish

--- src/quadpype/hosts/tvpaint/plugins/publish/collect_workfile.py
+++ src/quadpype/hosts/tvpaint/plugins/publish/collect_workfile.py
@@ -1,7 +1,11 @@
-import os
 import json
 import pyblish.api
+from pathlib import Path
+import tempfile
 
+from quadpype.hosts.tvpaint.api.lib import (
+    execute_george_through_file
+)
 
 class CollectWorkfile(pyblish.api.InstancePlugin):
     label = "Collect Workfile"
@@ -11,14 +15,15 @@ class CollectWorkfile(pyblish.api.InstancePlugin):
 
     def process(self, instance):
         context = instance.context
-        current_file = context.data["currentFile"]
+        current_file = Path(context.data["currentFile"])
 
         self.log.info(
-            "Workfile path used for workfile family: {}".format(current_file)
+            "Workfile path used for workfile family: {}".format(str(current_file))
         )
 
-        dirpath, filename = os.path.split(current_file)
-        basename, ext = os.path.splitext(filename)
+        filename = current_file.name
+        ext = current_file.suffix.lstrip(".")
+        dirpath = self._create_temp_workfile(filename)
 
         instance.data["representations"].append({
             "name": ext.lstrip(".").lower(),
@@ -27,6 +32,26 @@ class CollectWorkfile(pyblish.api.InstancePlugin):
             "stagingDir": dirpath
         })
 
+        instance.context.data["cleanupFullPaths"].append(dirpath)
+
         self.log.info("Collected workfile instance: {}".format(
             json.dumps(instance.data, indent=4)
         ))
+
+    @staticmethod
+    def _create_temp_workfile(filename):
+        """Create a temporary saved workfile.
+        It is important in case the user doesn't save before publish to have
+        the exact same file in the published workfile and the working workfile
+
+        Return: the new staging_dir"""
+
+        staging_dir = (
+            tempfile.mkdtemp(prefix="tvpaint_render_")
+        ).replace("\\", "/")
+
+        temp_wofkfile_path = Path(staging_dir) / filename
+        george_script = u"tv_SaveProject '{}'".format(str(temp_wofkfile_path))
+
+        execute_george_through_file(george_script)
+        return staging_dir
