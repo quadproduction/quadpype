4b0880b5bc231281ce12652d5a748bf45adb29ef
Guilhem Compain <guilhem.compain@quad.fr>
2025-03-21 19:08:06 +0100

    #98: bugfix: check for armatures in all child of empties instead of checking first object only

--- src/quadpype/hosts/blender/plugins/publish/extract_blend_animation.py
+++ src/quadpype/hosts/blender/plugins/publish/extract_blend_animation.py
@@ -37,18 +37,14 @@ class ExtractBlendAnimation(
 
         data_blocks = set()
 
-        for obj in instance:
-            if isinstance(obj, bpy.types.Object) and obj.type == 'EMPTY':
-                child = obj.children[0]
-                if child and child.type == 'ARMATURE':
-                    if child.animation_data and child.animation_data.action:
-                        if not obj.animation_data:
-                            obj.animation_data_create()
-                        obj.animation_data.action = child.animation_data.action
-                        obj.animation_data_clear()
-                        data_blocks.add(child.animation_data.action)
-                        data_blocks.add(obj)
-
+        for obj in self.get_empties(instance):
+            for armature in self.get_armatures_with_animation(obj.children):
+                if not obj.animation_data:
+                    obj.animation_data_create()
+                obj.animation_data.action = armature.animation_data.action
+                obj.animation_data_clear()
+                data_blocks.add(armature.animation_data.action)
+                data_blocks.add(obj)
         bpy.data.libraries.write(filepath, data_blocks, compress=self.compress)
 
         if "representations" not in instance.data:
@@ -64,3 +60,17 @@ class ExtractBlendAnimation(
 
         self.log.info("Extracted instance '%s' to: %s",
                        instance.name, representation)
+
+    @staticmethod
+    def get_empties(instance):
+        return [obj for obj in instance if isinstance(obj, bpy.types.Object) and obj.type == 'EMPTY']
+
+    @staticmethod
+    def get_armatures_with_animation(children):
+        return [
+            child for child in children if
+            child and
+            child.type == 'ARMATURE' and
+            child.animation_data and
+            child.animation_data.action
+        ]
