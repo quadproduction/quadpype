d825fbbbca61041b13627005892951c4daa56fe8
Guilhem Compain <guilhem.compain@quad.fr>
2025-06-17 18:49:33 +0200

    #206: bugfix: also load et set folder hierarchy when updating asset version

--- src/quadpype/hosts/aftereffects/plugins/load/load_file.py
+++ src/quadpype/hosts/aftereffects/plugins/load/load_file.py
@@ -110,34 +110,13 @@ class FileLoader(api.AfterEffectsLoader):
             self.log.warning("Check host app for alert error.")
             return
 
+        template_data, template_folder = self.get_folder_and_data_template(context['representation'])
+        if template_folder:
+            folders_hierarchy = self.get_folder_hierarchy(template_data, template_folder, unique_number)
+            self.create_folders(stub, folders_hierarchy, comp, stub.LOADED_ICON + comp_name)
+
         self[:] = [comp]
         namespace = namespace or comp_name
-        template_data = format_data(
-            original_data=context['representation'],
-            filter_variant=True,
-            app=get_current_host_name()
-        )
-        folder_templates = get_task_hierarchy_templates(
-            template_data,
-            task=get_current_context()['task_name']
-        )
-
-        if folder_templates:
-            folders_hierarchy = [
-                get_resolved_name(
-                    data=template_data,
-                    template=template,
-                    numbering=unique_number
-                )
-                for template in folder_templates
-            ]
-            create_folders_from_hierarchy(folders_hierarchy)
-            last_folder = find_folder(get_last_folder_from_first_template(folders_hierarchy))
-            stub.parent_items(comp.id, last_folder.id)
-            # if psd, must retrieve the folder to parent it too
-            if is_psd:
-                psd_folder = find_folder(stub.LOADED_ICON + comp_name)
-                stub.parent_items(psd_folder.id, last_folder.id)
 
         return api.containerise(
             name,
@@ -147,10 +126,43 @@ class FileLoader(api.AfterEffectsLoader):
             self.__class__.__name__
         )
 
+    @staticmethod
+    def get_folder_and_data_template(representation):
+        template_data = format_data(
+            original_data=representation,
+            filter_variant=True,
+            app=get_current_host_name()
+        )
+        return template_data, get_task_hierarchy_templates(
+            template_data,
+            task=get_current_context()['task_name']
+        )
+
+    def get_folder_hierarchy(self, template_data, folder_templates, unique_number):
+        return [
+            get_resolved_name(
+                data=template_data,
+                template=template,
+                numbering=unique_number
+            )
+            for template in folder_templates
+        ]
+
+    @staticmethod
+    def create_folders(stub, folders_hierarchy, comp, parent_folder_name):
+        create_folders_from_hierarchy(folders_hierarchy)
+        last_folder = find_folder(get_last_folder_from_first_template(folders_hierarchy))
+        stub.parent_items(comp.id, last_folder.id)
+
+        if parent_folder_name:
+            psd_folder = find_folder(parent_folder_name)
+            stub.parent_items(psd_folder.id, last_folder.id)
+
     def update(self, container, representation):
         """ Switch asset or change version """
         stub = self.get_stub()
         layer = container.pop("layer")
+        unique_number = container.get('version')
 
         context = representation.get("context", {})
 
@@ -183,6 +195,11 @@ class FileLoader(api.AfterEffectsLoader):
                        "name": context["subset"],
                        "namespace": layer_name}
         )
+
+        template_data, template_folder = self.get_folder_and_data_template(representation)
+        if template_folder:
+            folders_hierarchy = self.get_folder_hierarchy(template_data, template_folder, unique_number)
+            self.create_folders(stub, folders_hierarchy, layer, stub.LOADED_ICON + layer_name)
 
     def remove(self, container):
         """
