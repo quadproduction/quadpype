2b2e43cbbca990b89e210ee34cc94f1ae5695fd1
Guilhem Compain <guilhem.compain@quad.fr>
2025-12-17 12:17:10 +0100

    #436: feat: use win32gui + pyautogui in after effects autoclic scripts to better automatize .psd imports

--- src/quadpype/hosts/aftereffects/api/automate.py
+++ src/quadpype/hosts/aftereffects/api/automate.py
@@ -1,259 +1,341 @@
-import cv2
-import numpy as np
-import pyautogui
+import logging
 import time
-from mss import mss
-import platform
-import imutils
-from pathlib import Path
+import pyautogui
+import re
+
+import win32gui
+import win32con
 
 
-class Point:
-    x: int
-    y: int
+SMALL_OFFSET = 10
+LARGE_OFFSET = 50
+IMPORT_AS_COMP_OPTION_INDEX = 2  # Starts from 0
+ONLY_LETTERS_REGEX = r'[^a-zA-Z]'
+TRIES = 5
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
+IMPORT_FILE = "Importer fichier"
+IMPORT = "Importer sous :"
 
 
-class Shape:
-    width: int
-    height: int
+def try_several_times(function):
+    """ Try to execute decorator function a certain number of times (defined by TRIES constant).
+    Waiting time will gradually increase each loop to avoid large waiting times.
+    """
+    def wrapper(*args, **kwargs):
+        attempts = 0
+        while attempts < TRIES:
+            result = function(*args, **kwargs)
+            if result:
+                return result
 
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+            time_to_wait = 0.1 + (attempts / 10)
+            time.sleep(time_to_wait)
+            attempts += 1
 
-    def __add__(self, other):
-        return Shape(self.width + other.width, self.height + other.height)
+        return False
 
-    @property
-    def center(self):
-        return Point(self.width / 2, self.height / 2)
-
-
-class MatchResult:
-    anchor_point: Point
-    confidence: float
-    ratio: float
-    scale: float
-
-    def __init__(self, anchor_point, confidence, ratio, scale):
-        self.anchor_point = Point(anchor_point[0], anchor_point[1])
-        self.confidence = confidence
-        self.ratio = ratio
-        self.scale = scale
+    return wrapper
 
 
-class ElementCoordinates:
-    anchor_point: Point
-    shape: Shape
-    default_click: None
-
-    def __init__(self, point, shape):
-        self.anchor_point = point
-        self.shape = shape
-
-    def __add__(self, other):
-        return ElementCoordinates(self.anchor_point + other.anchor_point, self.shape + other.shape)
-
-    @property
-    def center(self):
-        return Point(self.anchor_point.x + self.shape.width / 2, self.anchor_point.y + self.shape.height / 2)
-
-    @property
-    def bottom(self):
-        return Point(self.anchor_point.x + self.shape.width / 2, self.anchor_point.y + self.shape.height / 1.2)
-
-    @property
-    def left(self):
-        return Point(self.anchor_point.x + self.shape.width / 8, self.anchor_point.y + self.shape.height / 2)
-
-    @property
-    def bottom_right(self):
-        return Point(self.anchor_point.x + self.shape.width / 1.2, self.anchor_point.y + self.shape.height / 1.2)
-
-    @property
-    def bottom_left(self):
-        return Point(self.anchor_point.x + self.shape.width / 8, self.anchor_point.y + self.shape.height / 1.2)
+@try_several_times
+def find_window_by_title(window_name):
+    return win32gui.FindWindow(None, window_name)
 
 
-class ClickableElement:
-    file_name: list[str]
-    folder_path: str
-    wait_after: float
-    threshold: float
-    default_click: str
+@try_several_times
+def find_window_by_partial_title(partial_title):
+    result = {'hwnd': None}
 
-    def __init__(self, files_names, folder_path, wait_after=0.0, threshold=0.5, click="center"):
-        self.files_names = files_names
-        self.folder_path = folder_path
-        self.wait_after = wait_after
-        self.threshold = threshold
-        self.default_click = click
+    def callback(hwnd, _):
+        if win32gui.IsWindowVisible(hwnd):
+            title = win32gui.GetWindowText(hwnd)
+            if title.startswith(partial_title):
+                result['hwnd'] = hwnd
+                return False
 
-        if not all([file_path.is_file() for file_path in self.files_paths]):
-            raise FileNotFoundError
+        return True
 
-    @property
-    def files_paths(self):
-        return [Path(self.folder_path, file_name) for file_name in self.files_names]
-
-    def file_path(self, file_name):
-        return Path(self.folder_path, file_name)
-
-    def get_file_name(self, file_path):
-        return getattr(file_path, "stem", Path(file_path).stem)
+    win32gui.EnumWindows(callback, None)
+    return result['hwnd']
 
 
-def get_combined_monitors_offset():
-    with mss() as sct:
-        return [sct.monitors[0]["left"], sct.monitors[0]["top"]]
+def force_foreground_window(hwnd):
+    try:
+        if not win32gui.IsWindow(hwnd):
+            logging.error("Given HWND is not valid.")
+            return False
+
+        if win32gui.IsIconic(hwnd):
+            win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)
+
+        win32gui.SetForegroundWindow(hwnd)
+        win32gui.SetActiveWindow(hwnd)
+
+        return True
+
+    except Exception as e:
+        print(f"An error has occured when forcing window to foreground : {e}")
+        return False
 
 
-def get_monitors_screenshot():
-    with mss() as sct:
-        screenshot = np.array(sct.grab(sct.monitors[0]))
-        return cv2.cvtColor(screenshot, cv2.COLOR_RGB2RGBA)
+def _pass_filter(hwnd, class_name, text):
+    """ Filter retrieved windows handle with class name and text.
+    We only keep letters from texts as specific characters (like special whitespaces) may be used in ui.
+    """
+    given_text = re.sub(ONLY_LETTERS_REGEX, '', text) if text else None
+    element_text = re.sub(ONLY_LETTERS_REGEX, '', win32gui.GetWindowText(hwnd)) if text else None
 
-
-def get_element_coordinates(template_path, screenshot, threshold, log, fixed_scale=None):
-    template_path = Path(template_path)
-    if not template_path.is_file():
-        log.error(f"Can not find file at path {template_path}.")
-        return
-
-    template = cv2.imread(str(template_path), cv2.IMREAD_COLOR)
-    template = cv2.cvtColor(template, cv2.COLOR_RGB2RGBA)
-    template_height, template_width = template.shape[:2]
-
-    found = get_match(
-        scales=[fixed_scale] if fixed_scale else np.linspace(0.25, 1.0, 20)[::-1],
-        screenshot=screenshot,
-        template=template,
-        log=log
+    return (
+        (not class_name or win32gui.GetClassName(hwnd) == class_name) and
+        (not text or given_text == element_text)
     )
 
-    if _match_is_not_valid(found, threshold) and fixed_scale:
-        log.debug(
-            f"No match found with previous memorized scale ({fixed_scale}). Will try again with all available spaces."
-        )
-        found = get_match(
-            scales=np.linspace(0.25, 1.0, 20)[::-1],
-            screenshot=screenshot,
-            template=template,
-            log=log
-        )
 
-    if _match_is_not_valid(found, threshold):
-        log.warning(f"Can not found {template_path.stem} in user monitor.")
-        return None, None
+def get_controls(hwnd_parent, filter_by_class=None, filter_by_text=None, verbose=False):
+    """ Get controls in given window, based on class or text (or nothing to get all existing and visible controls).
 
-    return ElementCoordinates(
-        point=Point(int(found.anchor_point.x * found.ratio), int(found.anchor_point.y * found.ratio)),
-        shape=Shape(template_width * found.ratio, template_height * found.ratio)
-    ), found.scale
+    Arguments:
+        hwnd_parent (int): Window handle (hwnd) in which to look out for file selection view.
+        filter_by_class (str): Get specific controls with class
+        filter_by_class (str): Get specifics controls with text
+        verbose (bool): display more details regarding analyzed elements.
+
+    Returns:
+        object: Control corresponding to file selection view.
+    """
+    controls = []
+    controls_logs = []
+
+    def callback(hwnd, _):
+        try:
+            class_name = win32gui.GetClassName(hwnd)
+            if not _pass_filter(hwnd, filter_by_class, filter_by_text):
+                return
+
+            visible = win32gui.IsWindowVisible(hwnd)
+            if not visible:
+                return
+
+            controls.append(hwnd)
+
+            if not verbose:
+                return
+
+            text = win32gui.GetWindowText(hwnd)
+            rect = win32gui.GetWindowRect(hwnd)
+            enabled = win32gui.IsWindowEnabled(hwnd)
+            width = rect[2] - rect[0]
+            height = rect[3] - rect[1]
+            controls_logs.append({
+                'hwnd': hwnd,
+                'class': class_name,
+                'text': text,
+                'rect': rect,
+                'size': (width, height),
+                'visible': visible,
+                'enabled': enabled
+            })
+
+        except Exception as e:
+            logging.error(f"Error with hwnd {hwnd}: {e}")
+        return True
+
+    # Callback for recursive items
+    try:
+        win32gui.EnumChildWindows(hwnd_parent, callback, None)
+
+    except TypeError:
+        pass
+
+    # Sort by global size
+    controls.sort(
+        key=lambda c: (
+            (win32gui.GetWindowRect(c)[2] - win32gui.GetWindowRect(c)[0]) +
+            (win32gui.GetWindowRect(c)[3] - win32gui.GetWindowRect(c)[1])
+        ),
+        reverse=True
+    )
+
+    if not verbose:
+        return controls
+
+    print(f"Total number of controls found: {len(controls)}")
+
+    for i, ctrl in enumerate(controls_logs):
+        print(f"[{i:3d}] Class: {ctrl['class']:35} | "
+              f"Text: {ctrl['text'][:40]:40} | "
+              f"Size: {ctrl['size'][0]:4}x{ctrl['size'][1]:<4} | "
+              f"Pos: ({ctrl['rect'][0]}, {ctrl['rect'][1]}) | "
+              f"Enabled: {ctrl['enabled']}")
+
+    print('\n')
+    return controls
 
 
-def _match_is_not_valid(found, threshold):
-    return found is None or found.confidence < threshold
+def get_file_selection_view(hwnd_parent, verbose=False):
+    """ Get file selection view from given window.
+    We assume that this is the second larger DirectUIHWND of the given window, as the first one seems
+    to always correspond to the global window.
 
+    Arguments:
+        hwnd_parent (int): Window handle (hwnd) in which to look out for file selection view.
+        verbose (bool): display more details regarding analyzed elements.
 
-def get_match(scales, screenshot, template, log):
-    template_height, template_width = template.shape[:2]
-    found = None
-
-    for scale in scales:
-        resized = imutils.resize(screenshot, width=int(screenshot.shape[1] * scale))
-        ratio = screenshot.shape[1] / float(resized.shape[1])
-
-        if resized.shape[0] < template_height or resized.shape[1] < template_width:
-            log.warning("Resized screenshot is too large in comparison of given image. Process will be stopped.")
-            break
-
-        result = cv2.matchTemplate(resized, template, cv2.TM_CCOEFF_NORMED)
-        _, confidence, _, location = cv2.minMaxLoc(result)
-        match_result = MatchResult(
-            anchor_point=location,
-            confidence=confidence,
-            ratio=ratio,
-            scale=scale
-        )
-
-        if not found or match_result.confidence > found.confidence:
-            found = match_result
-
-    return found
-
-
-def move_cursor_and_click(coordinates, offset, log, click="center"):
-    if not coordinates:
-        log.error("Can not move cursor because coordinates are null.")
+    Returns:
+        object: Control corresponding to file selection view.
+    """
+    direct_huiwnd_windows = get_controls(hwnd_parent, filter_by_class="DirectUIHWND", verbose=verbose)
+    windows_number = len(direct_huiwnd_windows)
+    if not windows_number >= 2:
+        logging.error(f"Correct window should be the second of found windows, but only {windows_number} have been found.")
         return
-    click_coordinates = getattr(coordinates, click)
-    pyautogui.moveTo(offset[0] + click_coordinates.x, offset[1] + click_coordinates.y)
-    pyautogui.click()
+
+    return direct_huiwnd_windows[1]
+
+
+def click_on_element(hwnd_list, offset=SMALL_OFFSET):
+    if isinstance(offset, list) or isinstance(offset, tuple):
+        x_offset, y_offset = offset
+
+    else:
+        x_offset = y_offset = offset
+
+    rect = win32gui.GetWindowRect(hwnd_list)
+
+    pyautogui.click(rect[0] + x_offset, rect[1] + y_offset)
+
     return True
 
 
-def import_file_dialog_clic(log):
-    screen_offset = get_combined_monitors_offset()
-    folder_path = Path(__file__).parent / "resources" / "auto_click" / platform.system().lower()
+def get_combobox(hwnd_parent, text, verbose=False):
+    """ Retrieve combobox in given window based on given text
 
-    assert folder_path.is_dir(), "Folder containing image ressources used for comparison can not be found."
+    Arguments:
+        hwnd_parent (int): Window handle (hwnd) in which to look out for file selection view.
+        text (str): Text corresponding to the label up to the desired combobox.
+        verbose (bool): display more details regarding analyzed elements.
 
-    elements_to_click = list()
+    Returns:
+        object: Control corresponding to file selection view.
+    """
+    comboboxes = get_controls(hwnd_parent, filter_by_class="ComboBox", verbose=verbose)
+    if not comboboxes:
+        logging.error("Can not find any combobox in import window.")
+        return
 
-    try:
-        elements_to_click.append(
-            ClickableElement(
-                files_names=["photoshop_file_icon_1.png", "photoshop_file_icon_2.png"],
-                folder_path=folder_path,
-                click="bottom_right"
-            )
-        )
-        elements_to_click.append(ClickableElement(["metrage.png"], folder_path, click="bottom"))
-        elements_to_click.append(ClickableElement(["composition.png"], folder_path, click="bottom", wait_after=0.3))
-        elements_to_click.append(ClickableElement(["importer.png"], folder_path, click="bottom_left", wait_after=0.3))
-        elements_to_click.append(ClickableElement(["ok.png"], folder_path, click="left"))
+    if verbose:
+        print(f"Comboboxes found : {len(comboboxes)}")
+        for i, cb in enumerate(comboboxes):
+            print(f"  [{i}] {win32gui.GetClassName(cb)} - {win32gui.GetWindowText(cb)} - Y:{win32gui.GetWindowRect(cb)[1]}")
+        print('\n')
 
-    except FileNotFoundError as err:
-        log.error("An error has occured when retrieving image for comparison. Abort process.")
-        log.error(err)
-        return False
+    labels = get_controls(hwnd_parent, filter_by_class="Static", filter_by_text=text, verbose=verbose)
+    if not labels:
+        logging.error("Can not find any label in import window.")
+        return
 
-    scale_used = None
-    for concerned_element in elements_to_click:
-        element_coordinates = None
+    if verbose:
+        print(f"Labels found : {len(labels)}")
+        for i, lb in enumerate(labels):
+            print(
+                f"  [{i}] {win32gui.GetClassName(lb)} - {win32gui.GetWindowText(lb)}")
+        print('\n')
 
-        for file_path in concerned_element.files_paths:
-            file_name = concerned_element.get_file_name(file_path)
-            log.debug(f"Will try to click on element named {file_name}.")
-            element_coordinates, scale_used = get_element_coordinates(
-                file_path,
-                get_monitors_screenshot(),
-                concerned_element.threshold,
-                log,
-                fixed_scale=scale_used
-            )
+    label = labels[0]
 
-            if not element_coordinates:
-                continue
+    label_y = win32gui.GetWindowRect(label)[1]
 
-            move_cursor_and_click(element_coordinates, screen_offset, log, click=concerned_element.default_click)
-            log.info(f"Button named {file_name} has ben correctly clicked.")
+    # Get nearby combobox horizontally
+    nearby = [
+        cb for cb in comboboxes
+        if abs(win32gui.GetWindowRect(cb)[1] - label_y) < 40
+    ]
+    print(nearby)
+    if not nearby:
+        logging.error("Can not find any nearby combobox.")
+        return
 
-            if concerned_element.wait_after:
-                time.sleep(concerned_element.wait_after)
+    return nearby[0]
 
-            break
 
-        if not element_coordinates:
-            log.error(f"Process has been aborted because button was not found.")
-            return
+def select_item_from_opened_combobox(item_number):
+    for _ in range(item_number):
+        pyautogui.press('down')
+
+
+def press_enter_key():
+    pyautogui.press('enter')
+
+
+def get_button(hwnd_parent, button_label, verbose):
+    buttons = get_controls(hwnd_parent, filter_by_class="Button", filter_by_text=button_label, verbose=verbose)
+    if not buttons:
+        logging.error(f"Can not find any button labelized '{button_label}'.")
+        return
+
+    if len(buttons) > 1:
+        logging.warning(f"More than 1 button find with label '{button_label}'. First one will be used.")
+
+    return buttons[0]
+
+
+def import_file_dialog_clic(file_name, verbose=False):
+    """ Automatically import file as Composition through After Effects window loader.
+    This function is using win32gui to retrieve opened windows and UI element + pyautogui to
+    simulate user clicks and key events.
+    The followed steps are :
+    1. Get main import window and put it foreground
+    2. Get files treeview and click on first element
+    3. Select import type combobox, click on it and select the third option with down arrow
+    4. Press enter key to validate previous choice
+    5. Press enter key to validate import
+    6. Wait for second import window to appears by tring to retrieve it
+    7. Press enter key to finalize import
+
+    Arguments:
+        file_name (str): Imported file name without frame information.
+        verbose (bool): display more details regarding analyzed elements.
+
+    Returns:
+        bool: True if success, else None
+    """
+
+    # Get main window and put in foreground
+    ae_import_window = find_window_by_title(IMPORT_FILE)
+    if not ae_import_window:
+        logging.error("Can not find import window.")
+        return
+
+    if not force_foreground_window(ae_import_window):
+        logging.error("Can not force given window to foreground.")
+        return
+
+    # Retrieve files list window and click on first element
+    file_selection_window = get_file_selection_view(ae_import_window, verbose=verbose)
+    if not file_selection_window:
+        logging.error("Can not find file selection window.")
+        return
+
+    click_on_element(file_selection_window, offset=LARGE_OFFSET)
+
+    # Get combobox and select third option
+    combobox_found = get_combobox(ae_import_window, IMPORT, verbose=verbose)
+    if not combobox_found:
+        logging.error(f"Can not find combobox with name '{IMPORT}' in current window.")
+        return
+
+    click_on_element(combobox_found)
+    select_item_from_opened_combobox(IMPORT_AS_COMP_OPTION_INDEX)
+
+    press_enter_key()  # Validate combobox option
+    press_enter_key()  # Validate import
+
+    # Wait for new window appearance and finalize import
+    new_import_window = find_window_by_partial_title(file_name)
+    if not new_import_window:
+        logging.error("Can not find second import window to finalize process.")
+        return
+
+    press_enter_key()  # Finalise import
 
     return True
