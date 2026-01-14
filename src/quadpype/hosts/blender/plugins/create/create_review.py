63d4a8ff63f108d2fb4dc0cccd43d5ea7d63250d
Guilhem Compain <guilhem.compain@quad.fr>
2026-01-14 16:11:32 +0100

    #524: feat: add resolution option to override playblast resolutions

--- src/quadpype/hosts/blender/plugins/create/create_review.py
+++ src/quadpype/hosts/blender/plugins/create/create_review.py
@@ -1,7 +1,11 @@
 """Create review."""
+import bpy
 
 from quadpype.hosts.blender.api import plugin, lib
 from quadpype.lib import EnumDef, BoolDef, UISeparatorDef
+from quadpype.pipeline import get_current_project_name
+from quadpype.settings import get_project_settings
+from quadpype.pipeline.settings import get_available_resolutions, RES_SEPARATOR
 
 
 class CreateReview(plugin.BlenderCreator):
@@ -12,9 +16,15 @@ class CreateReview(plugin.BlenderCreator):
     family = "review"
     icon = "video-camera"
 
+    resolution = None
+    resolutions = []
+
     def create(
         self, subset_name: str, instance_data: dict, pre_create_data: dict
     ):
+        if pre_create_data.get("resolution"):
+            self.resolution = pre_create_data.get("resolution")
+
         # Run parent create method
         collection = super().create(
             subset_name, instance_data, pre_create_data
@@ -26,6 +36,37 @@ class CreateReview(plugin.BlenderCreator):
                 collection.objects.link(obj)
 
         return collection
+
+    def get_pre_create_attr_defs(self):
+        output = []
+
+        project_name = get_current_project_name()
+        project_settings = get_project_settings(project_name)
+        resolutions = get_available_resolutions(
+            project_name=project_name,
+            project_settings=project_settings
+        )
+
+        scene_resolution = (
+            f"{bpy.context.scene.render.resolution_x}"
+            f"{RES_SEPARATOR}"
+            f"{bpy.context.scene.render.resolution_y}"
+        )
+        if scene_resolution not in resolutions:
+            resolutions.append(scene_resolution)
+
+        if resolutions:
+            self.resolutions = resolutions
+            output.append(
+                EnumDef(
+                    "resolution",
+                    items=self.resolutions,
+                    default=scene_resolution,
+                    label="Resolution",
+                )
+            )
+
+        return output
 
     def get_instance_attr_defs(self):
         defs = lib.collect_animation_defs()
@@ -44,6 +85,21 @@ class CreateReview(plugin.BlenderCreator):
                     items=["active camera", "camera in instance", "viewport"],
                     default="active camera"
                 ),
+            ]
+        )
+
+        if self.resolutions:
+            defs.append(
+                EnumDef(
+                    "resolution",
+                    items=self.resolutions,
+                    default=self.resolution,
+                    label="Resolution",
+                )
+            )
+
+        defs.extend(
+            [
                 EnumDef(
                     "shader_mode",
                     label="Shader Mode",
