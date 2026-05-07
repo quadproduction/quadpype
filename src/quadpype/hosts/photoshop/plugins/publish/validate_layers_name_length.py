9f5cae18c9d477dd0c417cd429ac3e3f9dfbf17f
jadouu3dart <jadou3dart@gmail.com>
2026-05-07 14:30:38 +0200

    enhancement: 588: validator longueur nom layer psd

---
+++ src/quadpype/hosts/photoshop/plugins/publish/validate_layers_name_length.py
@@ -0,0 +1,77 @@
+import pyblish.api
+from quadpype.pipeline.publish import (
+    ValidateContentsOrder,
+    PublishXmlValidationError,
+    OptionalPyblishPluginMixin
+)
+from quadpype.hosts.photoshop import api as photoshop
+
+
+class ValidateLayersNameLengthSelect(pyblish.api.Action):
+    """Select the layers that have more than 31 characters"""
+
+    label = "Select Layers"
+    icon = "mouse-pointer"
+    on = "failed"
+
+    def process(self, context, plugin):
+        stub = photoshop.stub()
+        stub.select_layers(context.data['transientData'][plugin.__name__])
+
+        return True
+
+class ValidateLayersNameLengthCutName(pyblish.api.Action):
+    """Automatically rename the layer"""
+
+    label = "Cut Layer Name"
+    icon = "scissors"
+    on = "failed"
+
+    def process(self, context, plugin):
+
+        stub = photoshop.stub()
+        layers = context.data['transientData'][plugin.__name__]
+
+        for layer in layers:
+            new_name = layer.name[:31]
+            stub.rename_layer(layer.id, new_name)
+
+
+
+class ValidateLayersNameLength(
+        OptionalPyblishPluginMixin,
+        pyblish.api.ContextPlugin
+    ):
+    """Validate that layer names do not exceed 31 characters"""
+
+    label = "Validate Layers Name Length"
+    hosts = ["photoshop"]
+    order = ValidateContentsOrder
+    families = ["image"]
+    actions = [ValidateLayersNameLengthSelect, ValidateLayersNameLengthCutName]
+    optional = True
+    active = True
+
+    def process(self, context):
+        if not self.is_active(context.data):
+            return
+
+        return_list = list()
+        msg = f"\n\nThe layers name are too long:"
+
+        stub = photoshop.stub()
+        layers = stub.get_layers()
+
+        for layer in layers:
+            if len(layer.name) <= 31:
+                continue
+
+            return_list.append(layer)
+            msg += f"\n- {layer.name}"
+
+        if return_list:
+            if not context.data.get('transientData'):
+                context.data['transientData'] = dict()
+
+            context.data['transientData'][self.__class__.__name__] = return_list
+            raise PublishXmlValidationError(self, msg)
